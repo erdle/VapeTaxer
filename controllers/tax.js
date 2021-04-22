37b11458db135bf96a120dd91f74dd5ce92ce96e
Babken Mesropyan <babken@topsoft.am>
2021-04-22 15:04:01 +0400

    Added changed to single  tax product strategy'

--- controllers/tax.js
+++ controllers/tax.js
@@ -5,6 +5,8 @@ const TaxRate = require("../models/TaxRate")
 const Checkout = require("../models/Checkout")
 const Shop = require("../models/Shop")
 
+const TAX_PRODUCT_HANDLE = 'excise_tax'
+
 router.post(`/webhook`, async (ctx) => {
     const order = ctx.request.body
     //TODO filter tax products
@@ -58,6 +60,7 @@ router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
     const { name, accessToken } = shop
 
     try {
+
         const checkout_data = await Checkout.findOne({ token: id })
 
         const checkout_request = await (await fetch(`https://${name}/admin/api/2021-04/checkouts/${id}.json`, {
@@ -80,14 +83,14 @@ router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
             return
         }
 
-        const line_items = checkout_request.checkout.line_items;
+        const all_line_items = checkout_request.checkout.line_items;
 
-        const tax_items = await getTaxLineItems(checkout_request.checkout, "CA", shop)
-        const existing_line_items = line_items.filter(line_item => line_item.vendor != "ENDS_taxer").map(line_item => { return { variant_id: line_item.variant_id, quantity: line_item.quantity } })
-        
+
+        const existing_line_items = all_line_items.filter(line_item => line_item.vendor != "ENDS_taxer").map(line_item => { return { variant_id: line_item.variant_id, quantity: line_item.quantity } })
+        const tax_line_item = await getTaxLineItem(checkout_request.checkout, shop)
         const line_items_with_tax = [
+            tax_line_item,
             ...existing_line_items,
-            ...tax_items
         ]
 
         const update_checkout_request = await (await fetch(`https://${name}/admin/api/2021-04/checkouts/${id}.json`, {
@@ -130,62 +133,21 @@ router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
 
 })
 
-async function getTaxLineItems(checkout, state, shop) {
+async function calculateTotalTax(checkout, state, shop) {
 
     const line_items = checkout.line_items;
 
     const tax_rates = await TaxRate.find({ "state.shortcode": state, shop: shop.name })
 
-    let calculated_taxes = []
+    let total_tax = 0
     for (const line_item of line_items) {
-        const line_item_taxes = await calcProductTaxes(line_item, tax_rates, shop)
-        calculated_taxes = [...line_item_taxes, ...calculated_taxes]
+        total_tax += await calcLineTaxes(line_item, tax_rates, shop)
     }
 
-    if(calculated_taxes.length == 0)
-        return []
-
-    const variants = calculated_taxes.map(calculated_tax => {
-        return {
-            "option1": calculated_tax.title,
-            "price": calculated_tax.tax_value,
-            "inventory_management": "shopify",
-            "inventory_quantity": calculated_tax.quantity,
-            "taxable": false
-        }
-    })
-
-    const new_tax_product_data = {
-        product: {
-            "title": `Tax product, checkout token -- ${checkout.token}`,
-            "vendor": "ENDS_taxer",
-            "product_type": "ENDS_tax",
-            "variants": variants,
-            "tags": "tax_product"
-        }
-    };
-
-    const tax_product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products.json`, {
-        method: 'POST',
-        headers: {
-            'X-Shopify-Access-Token': shop.accessToken,
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(new_tax_product_data)
-    })).json()
-
-    const tax_product = tax_product_request.product;
-    const tax_line_items = tax_product.variants.map(variant => {
-        return {
-            variant_id: variant.id,
-            quantity: variant.inventory_quantity
-        }
-    })
-
-    return tax_line_items;
+    return total_tax;
 }
 
-async function calcProductTaxes(line_item, tax_rates, shop) {
+async function calcLineTaxes(line_item, tax_rates, shop) {
 
     const product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products/${line_item.product_id}.json`, {
         method: 'GET',
@@ -201,24 +163,19 @@ async function calcProductTaxes(line_item, tax_rates, shop) {
     }
 
     const product = product_request.product
-    const taxes = []
+    let tax = 0;
     for (const state_tax of tax_rates) {
         if (product.tags.indexOf(state_tax.tax.tag) > -1) {
             const variant = product.variants.find(variant => variant.id == line_item.variant_id)
             const tax_value = await calcConcreteTax(state_tax, variant, shop)
-            taxes.push({
-                title: `${product.title} ${variant.title}`,
-                quantity: line_item.quantity,
-                tax_value
-            })
+            tax += line_item.quantity * tax_value;
         }
     }
 
-    return taxes;
+    return tax;
 }
 
 async function calcConcreteTax(state_tax, variant, shop) {
-
     //TODO add per ML logic
     switch (state_tax.taxType) {
         case 'cost_percent': {
@@ -234,11 +191,11 @@ async function calcConcreteTax(state_tax, variant, shop) {
                 throw inventory_item_request.errors
             }
             const { inventory_item } = inventory_item_request
-            const tax_value = Number(inventory_item.cost) * (state_tax.value / 100)
+            const tax_value = Number(inventory_item.cost) * state_tax.value
             return tax_value;
         }
         case 'price_percent': {
-            const tax_value = Number(variant.price) * (state_tax.value / 100)
+            const tax_value = Number(variant.price) * state_tax.value
             return tax_value
         }
         case 'item_fixed': {
@@ -248,9 +205,67 @@ async function calcConcreteTax(state_tax, variant, shop) {
             //TODO get ML
             return state_tax.value
         }
-
     }
 }
 
+async function getTaxLineItem(checkout, shop) {
+
+    const total_tax = await calculateTotalTax(checkout, "CA", shop)
+    const tax_product = (await getTaxProduct(shop)) || (await createTaxProduct(shop));
+    const variant_id = tax_product && tax_product.variants && tax_product.variants[0] && tax_product.variants[0].id;
+
+    return {
+        variant_id: variant_id,
+        properties: { 'cost_percent': 1000 },
+        quantity: 1
+    };
+}
+
+async function getTaxProduct(shop) {
+    const tax_product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products.json?handle=${TAX_PRODUCT_HANDLE}`, {
+        method: 'GET',
+        headers: {
+            'X-Shopify-Access-Token': shop.accessToken,
+            'content-type': 'application/json'
+        }
+    })).json()
+    if (tax_product_request.errors) {
+        throw tax_product_request.errors
+    }
+    const tax_product = tax_product_request.products && tax_product_request.products.length > 0 && tax_product_request.products[0];
+    return tax_product;
+}
+
+async function createTaxProduct(shop) {
+    const new_tax_product_data = {
+        product: {
+            "title": `Tax product`,
+            "handle": TAX_PRODUCT_HANDLE,
+            "vendor": "ENDS_taxer",
+            "product_type": "ENDS_tax",
+            "tags": "tax_product",
+            "variants": [{
+                "option1": "Tax",
+                "price": 1000000,
+                "inventory_management": "shopify",
+                "inventory_policy": "continue",
+                "taxable": false
+            }]
+        }
+    };
+
+    const new_tax_product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products.json`, {
+        method: 'POST',
+        headers: {
+            'X-Shopify-Access-Token': shop.accessToken,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(new_tax_product_data)
+    })).json()
+    if (new_tax_product_request.errors) {
+        throw new_tax_product_request.errors
+    }
+    return new_tax_product_request.product
+}
 
 module.exports = router
