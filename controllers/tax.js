71d47d215f63e20a669c0674badfb31a95cd8664
Babken Mesropyan <babken@topsoft.am>
2021-04-27 23:52:38 +0400

    Added tax lines

--- controllers/tax.js
+++ controllers/tax.js
@@ -4,6 +4,7 @@ const router = new Router();
 const TaxRate = require("../models/TaxRate")
 const Checkout = require("../models/Checkout")
 const Shop = require("../models/Shop")
+const TaxLine = require("../models/TaxLine")
 
 const TAX_PRODUCT_HANDLE = 'excise_tax'
 
@@ -64,6 +65,15 @@ router.get(`/cleartax/:shop_name/:id`, cors(), async (ctx) => {
         ctx.status = 500;
         ctx.body = e;
     }
+})
+
+router.get(`/calctax/:shop_name/:variant_id/:state`, cors(), async (ctx) => {
+    const { variant_id, shop_name, state } = ctx.params;
+    const shop = await Shop.findOne({ name: shop_name })
+    const { name, accessToken } = shop
+    const result = await calculateTotalTaxByVariant(variant_id, state, shop)
+    ctx.status = 200;
+    ctx.body = { tax: result };
 })
 
 router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
@@ -136,7 +146,7 @@ router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
                     updated_at: new Date(update_checkout_request.checkout.updated_at)
                 }
             },
-            { upsert: true }).exec();
+            { upsert: true }).exec()
 
         ctx.status = 200;
         ctx.body = { updated_at: update_checkout_request.checkout.updated_at };
@@ -148,7 +158,52 @@ router.get(`/addtaxes/:shop_name/:id`, cors(), async (ctx) => {
 
 })
 
-async function calculateTotalTax(checkout, state, shop) {
+
+async function calculateTotalTaxByVariant(variantId, state, shop) {
+
+    const tax_rates = await TaxRate.find({ "state.shortcode": state, shop: shop.name })
+
+    const variant_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/variants/${variantId}.json`, {
+        method: 'GET',
+        headers: {
+            'X-Shopify-Access-Token': shop.accessToken,
+            'content-type': 'application/json'
+        }
+    })).json()
+
+
+    if (variant_request.errors) {
+        console.log(variant_request.errors)
+        throw variant_request.errors
+    }
+    const variant = variant_request.variant
+
+    const product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products/${variant.product_id}.json`, {
+        method: 'GET',
+        headers: {
+            'X-Shopify-Access-Token': shop.accessToken,
+            'content-type': 'application/json'
+        }
+    })).json()
+
+    if (product_request.errors) {
+        console.log(product_request.errors)
+        throw product_request.errors
+    }
+
+    const product = product_request.product
+    let tax = 0;
+    for (const state_tax of tax_rates) {
+        if (product.tags.indexOf(state_tax.tax.tag) > -1) {
+            const tax_value = await calcConcreteTax(state_tax, variant, product, shop)
+            tax += tax_value;
+        }
+    }
+
+    return tax;
+}
+
+async function calculateTotalTaxByCheckout(checkout, state, shop) {
 
     const line_items = checkout.line_items;
 
@@ -156,13 +211,13 @@ async function calculateTotalTax(checkout, state, shop) {
 
     let total_tax = 0
     for (const line_item of line_items) {
-        total_tax += await calcLineTaxes(line_item, tax_rates, shop)
+        total_tax += await calcLineTaxes(line_item, tax_rates, checkout.token, shop)
     }
 
     return total_tax;
 }
 
-async function calcLineTaxes(line_item, tax_rates, shop) {
+async function calcLineTaxes(line_item, tax_rates, token, shop) {
 
     const product_request = await (await fetch(`https://${shop.name}/admin/api/2021-04/products/${line_item.product_id}.json`, {
         method: 'GET',
@@ -182,15 +237,24 @@ async function calcLineTaxes(line_item, tax_rates, shop) {
     for (const state_tax of tax_rates) {
         if (product.tags.indexOf(state_tax.tax.tag) > -1) {
             const variant = product.variants.find(variant => variant.id == line_item.variant_id)
-            const tax_value = await calcConcreteTax(state_tax, variant, shop)
+            const tax_value = await calcConcreteTax(state_tax, variant, product, shop)
             tax += line_item.quantity * tax_value;
+
+            await TaxLine.create({
+                checkout_id: token,
+                variant_id: line_item.id,
+                tax_rate_id: state_tax._id,
+                value: tax_value,
+                quantity: line_item.quantity,
+                shop: shop.name,
+            });
         }
     }
 
     return tax;
 }
 
-async function calcConcreteTax(state_tax, variant, shop) {
+async function calcConcreteTax(state_tax, variant, product, shop) {
     //TODO add per ML logic
     switch (state_tax.taxType) {
         case 'cost_percent': {
@@ -214,28 +278,53 @@ async function calcConcreteTax(state_tax, variant, shop) {
             return tax_value
         }
         case 'item_fixed': {
-            return state_tax.value
+            return state_tax.value * 100
+        }
+        case 'per_pod_or_cartridge': {
+            const option_values = [variant.option1 && variant.option1.toLocaleLowerCase(), variant.option2 && variant.option2.toLocaleLowerCase(), variant.option3 && variant.option3.toLocaleLowerCase()]
+            const packs_title = option_values && option_values.find(op => op.indexOf("pack of") > -1)
+            const variant_packs = Number(packs_title && packs_title.replace("pack of", ""))
+            const product_title_packs = Number(product.title.split("pack of") && product.title.split("pack of")[1])
+            if (typeof variant_packs == "number")
+                return state_tax.value * variant_packs * 100
+            else if (typeof product_title_packs == "number")
+                return state_tax.value * product_title_packs * 100
+            else
+                return state_tax.value * 3 * 100
         }
         case 'ml_fixed': {
-            //TODO get ML
-            return state_tax.value
+            const regex = /(\d+(?:.\d+)?)\s*(ml)\b/i;
+            const ml_from_variant_title = getMLfromString(variant.title)
+            const ml_from_product = getMLfromString(product.title)
+            if (ml_from_variant_title)
+                return state_tax.value * ml_from_variant_title * 100
+            else if (ml_from_product)
+                return state_tax.value * ml_from_product * 100
+            else
+                return state_tax.value * 60 * 100
         }
     }
 }
 
-async function getTaxLineItem(checkout, shop) {
+function getMLfromString(text) {
+    const regex = /(\d+(?:.\d+)?)\s*(ml)\b/i;
 
-    const total_tax = await calculateTotalTax(checkout, "CA", shop)
+    const reg_match = text.match(regex)
+    const result = Number(reg_match && reg_match[1])
+    return typeof result == "number" && result
+}
+
+async function getTaxLineItem(checkout, shop) {
+    const total_tax = await calculateTotalTaxByCheckout(checkout, "CA", shop)
     const tax_product = (await getTaxProduct(shop)) || (await createTaxProduct(shop));
     const variant_id = tax_product && tax_product.variants && tax_product.variants[0] && tax_product.variants[0].id;
 
     return {
         variant_id: variant_id,
-        properties: { '_cost_percent': total_tax },
-        price: 123222,
+        properties: { 'excise_tax': total_tax },
+        price: 1000000,
         quantity: 1
     };
-
 }
 
 async function getTaxProduct(shop) {
